5531b1601771e4b191816f161ae8986ad305607c
Unknown <pvedoya@itba.edu.ar>
2018-06-23 17:34:25 -0300

    tp

---
+++ src/test/java/TPE/Timbiriche/AppTest.java
@@ -0,0 +1,12 @@
+package TPE.Timbiriche;
+
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest{
+    public static void main(String[] args){
+        GameManager gm = new GameManager(4,0,0,0,false);
+
+    }
+}
---
+++ src/test/java/TPE/Timbiriche/IsItAliveTest.java
@@ -0,0 +1,69 @@
+package TPE.Timbiriche;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.IOException;
+
+import static org.junit.Assert.assertEquals;
+
+public class IsItAliveTest {
+
+    private Board board;
+    private int size = 4;
+
+    @Before
+    public void before(){
+        board = new Board(size);
+    }
+
+    @Test
+    public void evaluateAvailableTest(){
+        assertEquals(24,board.availableMoves());
+    }
+
+
+    @Test
+    public void isItAliveDepthTest(){
+        AIPlayer cortana = new AIPlayer(1,2, 1,3,true);
+
+        board.makeMove(new Line(0,1,1,1),1);
+        board.makeMove(new Line(0,0,0,1),1);
+        board.makeMove(new Line(0,0,1,0),1);
+        board.makeMove(new Line(2,2,3,2),1);
+        board.makeMove(new Line(2,2,2,3),1);
+        board.printBoard();
+
+
+        Line line = null;
+        try {
+            line = cortana.miniMax(board);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        board.makeMove(line,1);
+
+
+
+
+        board.printBoard();
+    }
+
+    @Test
+    public void isItAliveTimeTest(){
+        AIPlayer cortana = new AIPlayer(1,2, 0,4,true);
+        long i = System.currentTimeMillis();
+        Line line = null;
+        try {
+            line = cortana.miniMax(board);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        i = System.currentTimeMillis() - i;
+        System.out.print(i);
+        System.out.print('\n');
+        board.makeMove(line,1);
+
+        board.printBoard();
+    }
+}
